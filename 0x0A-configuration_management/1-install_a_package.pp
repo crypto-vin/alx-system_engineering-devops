07a951a618b863d216c51f61c7b34654c221fc48
vin <44378566+crypto-vin@users.noreply.github.com>
2023-11-15 16:17:17 +0300

    solved task #1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,25 @@
-#install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
 }
+
+package { 'build-essential':
+  ensure => installed,
+}
+
+package { 'libssl-dev':
+  ensure => installed,
+}
+
+package { 'libffi-dev':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin',
+  unless  => '/usr/local/bin/flask --version | grep "Flask 2.1.0"',
+}
+
+
